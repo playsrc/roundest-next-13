323ad70bbd9d12d99c4223dac6f5ba71e0cb394c
Mateus Abelli <mateusabelli@gmail.com>
2023-01-07 15:13:15 -0300

    Remove data fetching from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,18 @@
+import { Suspense } from "react";
 import ComparisonBox from "../components/ComparisonBox";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { db } from "../lib/db";
-import { getRandomIds } from "../lib/get-random-ids";
 
 import styles from "./page.module.css";
 
-export const dynamic = "force-dynamic";
-
-async function getPokemon() {
-  const { firstId, secondId } = getRandomIds();
-
-  const firstPokemon = await db.pokemon.findFirst({ where: { id: firstId } });
-  const secondPokemon = await db.pokemon.findFirst({ where: { id: secondId } });
-
-  return { firstPokemon, secondPokemon };
-}
-
 export default async function Home() {
-  const { firstPokemon, secondPokemon } = await getPokemon();
-  if (!firstPokemon || !secondPokemon) return;
-
   return (
     <div className={styles.container}>
       <Header text="Which Pokémon is Rounder?" />
-
-      <ComparisonBox pokemons={{ firstPokemon, secondPokemon }} />
-
+      <Suspense fallback={<h1>Loading...</h1>}>
+        {/* @ts-ignore */}
+        <ComparisonBox />
+      </Suspense>
       <Footer />
     </div>
   );
